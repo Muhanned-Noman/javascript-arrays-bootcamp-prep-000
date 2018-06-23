9b82de85698492522abe0fed305b47eb164f7bfc
Muhanned Abdulhaq Mohammed Noman <muhanned.noman@gmail.com>
2018-06-23 12:27:54 +0000

    automatically backed up by learn

--- arrays.js
+++ arrays.js
@@ -1,7 +1,10 @@
 var chocolateBars = ["snickers","hundred grand","kitkat","skittles"]
 
-function addElementToBeginningOfArray(){
+function addElementToBeginningOfArray(array,elment){
   
 }
 
-function 
+function destructivelyAddElementToBeginningOfArray(array,elment){
+  
+}
+
